4778221b03f06bd76175100fc9924cb8b9c07f9a
Paul <comrade_pasha@ukr.net>
2016-11-28 01:51:47 +0200

    c6

--- platforms/android/assets/www/myapp.js
+++ platforms/android/assets/www/myapp.js
@@ -1,15 +1,28 @@
 /**
  * Created by paul on 24.11.16.
  */
-function buttonClick(){
-    console.log("Message here!");
-}
 
-document.addEventListener("deviceready", function(){
-    alert("Device is Ready!");
-
-    document.addEventListener("backbutton", function(){
-       alert("Back button pressed!");
-    });
+document.addEventListener("deviceready", function () {
+    console.log("Device is ready!");
+    //StatusBar.hide();
+    StatusBar.overlaysWebView(false);
+    StatusBar.backgroundColorByName("red");
 });
+    
+function notifyMe(){
+    alert("Notified!");
+}
+    
+    
+    
+    //    alert("Device is Ready!");
+
+//    document.addEventListener("backbutton", function(){
+//       alert("Back button pressed!");
+
+//function buttonClick(){
+//    console.log("Message here!");
+//}
+
+
 
