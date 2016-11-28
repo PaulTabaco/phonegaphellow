a920113f993e316c2b8eaebbf2e603350a7916e5
Paul <comrade_pasha@ukr.net>
2016-11-28 18:23:05 +0200

    c8

--- platforms/ios/www/myapp.js
+++ platforms/ios/www/myapp.js
@@ -1,26 +1,66 @@
-/**
- * Created by paul on 24.11.16.
- */
-
 document.addEventListener("deviceready", function () {
     console.log("Device is ready!");
-    //StatusBar.hide();
     StatusBar.overlaysWebView(false);
     StatusBar.backgroundColorByName("red");
 });
     
-function notifyMe(){
+function notifyMe() {
     navigator.notification.alert("You'v been Notified!", null, "Header of Alert here", "Ok");
 }
     
-function confirmCallback(buttonInd){
-    navigator.notification.alert("Test " + buttonInd, null)
+function confirmDelCallback(buttonIndex) {
+    navigator.notification.alert("Test " + buttonIndex, null);
 }
 
-function confirmDelete(){
-    navigator.notification.confirm("Are you sure?", confirmCallback, "Header of Alert here", ["Delete","No"]);
+function confirmDelete() {
+    navigator.notification.confirm("Are you sure?", confirmDelCallback, "Header of Alert here", ["Delete", "No"]);
 }
     
+// PICTURE //
+
+function cameraFailed() {
+    navigator.notification.alert("Camera not aviable", null);
+}
+
+function takePicture() {
+    navigator.camera.getPicture(takePictureCallback, cameraFailed, 
+    {
+        quality: 80,
+        destinationType: Camera.DestinationType.DATA_URL,
+    });
+}
+
+function takePictureCallback(picture) {
+    var img = document.querySelector("#output img");
+    img.src = "data:image/jpg;base64,"+ picture;
+}
+
+/* SELFIE */
+
+function takeSelfie() {
+    navigator.camera.getPicture(takePictureCallback, cameraFailed, 
+    { 
+        quality: 80,
+        destinationType: Camera.DestinationType.DATA_URL,
+        saveToPhotoAlbum: true,
+        cameraDirection: Camera.Direction.FRONT
+    });
+}
+
+//
+//function takeSelfie(){
+//    navigator.camera.getPicture(takePictureCallback,cameraFailed, {
+//        quality:80,
+//        DestinationType:Camera.DestinationType.DATA_URL,
+//        SAVEDPHOTOALBUM:true,
+//        cameraDirection: Camera.direction.FRONT
+//    }     
+//)};
+
+
+
+
+
     
     //    alert("Device is Ready!");
 
--- www/myapp.js
+++ www/myapp.js
@@ -1,35 +1,66 @@
-/**
- * Created by paul on 24.11.16.
- */
-
 document.addEventListener("deviceready", function () {
     console.log("Device is ready!");
-    //StatusBar.hide();
     StatusBar.overlaysWebView(false);
     StatusBar.backgroundColorByName("red");
 });
     
-function notifyMe(){
+function notifyMe() {
     navigator.notification.alert("You'v been Notified!", null, "Header of Alert here", "Ok");
 }
     
-function confirmCallback(buttonIndex){
-    navigator.notification.alert("Test " + buttonIndex, null)
+function confirmDelCallback(buttonIndex) {
+    navigator.notification.alert("Test " + buttonIndex, null);
 }
 
-function confirmDelete(){
-    navigator.notification.confirm("Are you sure?", confirmCallback, "Header of Alert here", ["Delete","No"]);
+function confirmDelete() {
+    navigator.notification.confirm("Are you sure?", confirmDelCallback, "Header of Alert here", ["Delete", "No"]);
 }
     
-    
-    //    alert("Device is Ready!");
+// PICTURE //
 
-//    document.addEventListener("backbutton", function(){
-//       alert("Back button pressed!");
+function cameraFailed() {
+    navigator.notification.alert("Camera not aviable", null);
+}
 
-//function buttonClick(){
-//    console.log("Message here!");
-//}
+function takePicture() {
+    navigator.camera.getPicture(takePictureCallback, cameraFailed, 
+    {
+        quality: 80,
+        destinationType: Camera.DestinationType.DATA_URL,
+    });
+}
+
+function takePictureCallback(picture) {
+    var img = document.querySelector("#output img");
+    img.src = "data:image/jpg;base64,"+ picture;
+}
+
+/* SELFIE */
+
+function takeSelfie() {
+    navigator.camera.getPicture(takePictureCallback, cameraFailed, 
+    { 
+        quality: 80,
+        destinationType: Camera.DestinationType.DATA_URL,
+        saveToPhotoAlbum: true,
+        cameraDirection: Camera.Direction.FRONT
+    });
+}
 
 
+/* PICK FROM GALLERY */
 
+function pickPictureCallback(pictureURL) {
+    var img = document.querySelector("#output img");
+    img.src = pictureURL;
+}
+
+function pickPicture() {
+    navigator.camera.getPicture(pickPictureCallback, cameraFailed, 
+    { quality: 80,
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      correctOrientation: true  });
+}
